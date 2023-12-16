c464d989ee4dcb13fbe6a24407e37ba842380526
D:\Victor <nerf8.2012@gmail.com>
2023-12-16 13:26:02 +0100

    CollisionHandler.cs plays sound and re-/loads scenes, added overrides to rocket prefab

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,7 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {   
-    [SerializeField] float loadDelay = 1f;
+    [SerializeField] float loadDelay = 2f;
+    [SerializeField] AudioClip crashSFX;
+    [SerializeField] AudioClip successSFX;
+
+    AudioSource audioSource;
+    Movement movement;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        movement = GetComponent<Movement>();
+        movement.enabled = true;
+    }
 
     void OnCollisionEnter(Collision other)
     {
@@ -26,12 +38,32 @@ public class CollisionHandler : MonoBehaviour
 
     void StartCrashSequence()
     {   
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex);
+        movement.enabled = false;
+        audioSource.PlayOneShot(crashSFX);
+        Invoke("ReloadLevel", loadDelay);
     }
 
     void StartSuccessSequence()
     {
-        
+        movement.enabled = false;
+        audioSource.PlayOneShot(successSFX);
+        Invoke("LoadNextLevel", loadDelay);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
