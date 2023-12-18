6b12f3d4b4de761b4a75a02b85f10eef36aeb863
D:\Victor <nerf8.2012@gmail.com>
2023-12-18 21:48:28 +0100

    Finished Oscillator.cs

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -7,6 +7,7 @@ public class Oscillator : MonoBehaviour
     Vector3 startingPos;
     [SerializeField] Vector3 movementVector;
     [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField] float period = 2f;
 
     void Start()
     {
@@ -14,7 +15,14 @@ public class Oscillator : MonoBehaviour
     }
 
     void Update()
-    {
+    {   
+        if(period <= Mathf.Epsilon) { return; }
+        float cycles = Time.time / period; // continually growing over time
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+        movementFactor = (rawSinWave + 1f) / 2f;
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
